ce7d1fd99ddb66519750cb582b59b76d98e89902
jaydeep3428 <jaydeepmakwana80412@gmail.com>
2024-08-10 14:20:33 +0530

    remove the warnings

--- src/app/users/[userid]/update/page.js
+++ src/app/users/[userid]/update/page.js
@@ -10,7 +10,7 @@ export default function Page({ params }) {
 
   useEffect(() => {
     getUserDetails();
-  }, []);
+  });
 
   const getUserDetails = async () => {
     let data = await fetch("http://localhost:3000/api/users/" + id);
--- src/util/db.js
+++ src/util/db.js
@@ -1,35 +1,35 @@
-// export const user = [
-//   {
-//     Id: 1,
-//     Name: "Anil",
-//     Age: 20,
-//     Email: "anil@gmail.com",
-//   },
-//   {
-//     Id: 2,
-//     Name: "Sam",
-//     Age: 25,
-//     Email: "sam@gmail.com",
-//   },
-//   {
-//     Id: 3,
-//     Name: "Peter",
-//     Age: 18,
-//     Email: "peter@gmail.com",
-//   },
-//   {
-//     Id: 4,
-//     Name: "Butcher",
-//     Age: 35,
-//     Email: "butcher@gmail.com",
-//   },
-//   {
-//     Id: 5,
-//     Name: "John",
-//     Age: 30,
-//     Email: "john@gmail.com",
-//   },
-// ];
+export const user = [
+  {
+    Id: 1,
+    Name: "Anil",
+    Age: 20,
+    Email: "anil@gmail.com",
+  },
+  {
+    Id: 2,
+    Name: "Sam",
+    Age: 25,
+    Email: "sam@gmail.com",
+  },
+  {
+    Id: 3,
+    Name: "Peter",
+    Age: 18,
+    Email: "peter@gmail.com",
+  },
+  {
+    Id: 4,
+    Name: "Butcher",
+    Age: 35,
+    Email: "butcher@gmail.com",
+  },
+  {
+    Id: 5,
+    Name: "John",
+    Age: 30,
+    Email: "john@gmail.com",
+  },
+];
 
 //connection of mongodb
 
